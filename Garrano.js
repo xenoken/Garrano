ca1407e55d127d1f3c628ee1cc92156173c227ab
Ken Ekeoha <kenomaru@hotmail.it>
2019-06-10 03:49:14 +0200

    Update Garrano.js

--- Garrano.js
+++ Garrano.js
@@ -5,15 +5,15 @@ let Garrano = 		{
 	"Math"			:{
 		"isFinite"			: isFinite,
 		"isNan"				: isNaN,
-		"parseFloat"		: parseFloat,
+		"parseFloat"			: parseFloat,
 		"parseInt"			: parseInt,
 
 	},
 	"URI"			:{
 		"decode" 			: decodeURI,
-		"decodeComponent"	: decodeURIComponent,
+		"decodeComponent"		: decodeURIComponent,
 		"encode"			: encodeURI,
-		"encodeComponent"	: encodeURIComponent
+		"encodeComponent"		: encodeURIComponent
 	},
 	"escape"				: escape,
 	"unescape"				: unescape,
@@ -39,23 +39,23 @@ let Garrano = 		{
 	},
 	"Collections"	:{
 		"Array"		:{
-			"create_empty" 	: function() 				{	return []  					 	;},
-			"create"		: function(...elems) 		{	return new Array(...elems) 	 	;},
-			"item"			: function(arr,index) 		{	return arr[index] 			 	;},
-			"for_each"		: function(arr,func) 		{	return arr.forEach(func)  	 	;},
-			"add"			: function(arr,...elems) 	{	return arr.push(...elems)	 	;},
-			"remove_last"	: function(arr) 			{	return arr.pop()  			 	;},
-			"remove_first"	: function(arr) 			{	return arr.shift() 			 	;},
-			"add_front"		: function(arr,v) 			{	return arr.unshift(v) 		 	;},
-			"index_of" 		: function(arr,v) 			{	return arr.indexOf(v) 		 	;},
-			"splice"		: function(arr,pos,l) 		{	return arr.splice(pos,l) 	 	;},
-			"remove_at"		: function(arr,pos) 		{	return arr.splice(pos,1) 	 	;},
-			"clone"			: function(arr) 			{	return arr.slice() 			 	;},
-			"concat"		: function(arr,...values) 	{	return arr.concat(...values) 	;},
+			"create_empty" 		: function() 				{	return []  					 	;},
+			"create"		: function(...elems) 			{	return new Array(...elems) 	 		;},
+			"item"			: function(arr,index) 			{	return arr[index] 				;},
+			"for_each"		: function(arr,func) 			{	return arr.forEach(func)  	 		;},
+			"add"			: function(arr,...elems) 		{	return arr.push(...elems)	 		;},
+			"remove_last"		: function(arr) 			{	return arr.pop()  				;},
+			"remove_first"		: function(arr) 			{	return arr.shift() 				;},
+			"add_front"		: function(arr,v) 			{	return arr.unshift(v) 		 		;},
+			"index_of" 		: function(arr,v) 			{	return arr.indexOf(v) 		 		;},
+			"splice"		: function(arr,pos,l) 			{	return arr.splice(pos,l) 	 		;},
+			"remove_at"		: function(arr,pos) 			{	return arr.splice(pos,1) 	 		;},
+			"clone"			: function(arr) 			{	return arr.slice() 				;},
+			"concat"		: function(arr,...values) 		{	return arr.concat(...values) 			;},
 		},
 	},
 	"Types"			:{
-		"isArray" 			: function(o) 				{	return Array.isArray(o) 		;}
+		"isArray" 			: function(o) 				{	return Array.isArray(o) 			;}
 	},
 	"Prototype"		:{
 		"get"				: function(o) 				{ 	return o.prototype 				;}
